76bd925243994f1e111d05aa65c31af7019f577c
SamuelOlveraa <samuolvera332@gmail.com>
2025-06-20 14:45:51 -0600

    cambio para cachar error

--- backend/contact/index.js
+++ backend/contact/index.js
@@ -42,8 +42,8 @@ app.post('/api/contact', async (req, res) => {
 
     res.status(200).json({ success: true, message: 'Correo enviado correctamente' });
   } catch (err) {
-    console.error('❌ Error al enviar el correo:', err);
-    res.status(500).json({ error: 'Hubo un error al enviar el mensaje. Intenta más tarde.' });
+  console.error('Error al enviar el correo:', err);
+  res.status(500).json({ error: 'Error al enviar el mensaje', detail: err.message });
   }
 });
 
